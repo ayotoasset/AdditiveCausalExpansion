0c335f6631cc40b019321ec357cce6e7756c4619
mazphilip <philip@pilgerstorfer.at>
2018-03-11 00:15:30 +0000

    minor bugfix, using new kernel util header

--- src/stats_cpp.cpp
+++ src/stats_cpp.cpp
@@ -1,24 +1,28 @@
 // [[Rcpp::depends("RcppArmadillo")]]
 #include <RcppArmadillo.h>
-#include "ace_kernelutilities.hpp"
+#include "ace_kernel_utils.hpp"
 
 using namespace arma;
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-arma::rowvec stats_cpp(arma::colvec y, arma::mat& Kmat, arma::mat& invKmatn, arma::vec& eigenval, const double mu) {
+arma::rowvec stats_cpp(const arma::colvec& y,
+                       const arma::mat& Kmat,
+                       const arma::mat& invKmatn,
+                       const arma::vec& eigenval,
+                       const double mu) {
   //Rcpp::List gradients = clone(parameters); //for the same list structure
 
-  unsigned int n = y.size();
+  unsigned int n = y.n_rows;
   //preallocate memory
   arma::rowvec stats(2);
   arma::colvec alpha(n);
 
-  y = y - mu; //
-  alpha = invKmatn * y;
+  arma::colvec ybar = y - mu; //
+  alpha = invKmatn * ybar;
 
   //RMSE
-  stats(0) = pow(arma::norm(y - (Kmat * alpha)),2);
+  stats(0) = pow(arma::norm(ybar - (Kmat * alpha)),2);
   //Evidence
   stats(1) = logevidence(y, alpha, eigenval, n);
 
