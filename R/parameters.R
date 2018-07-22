bcfd1e2dd4ef892818fa38cccd69c061a4a686d4
mazphilip <philip@pilgerstorfer.at>
2018-07-22 16:17:12 +0100

    Bugfix printing of initial variance term

--- R/parameters.R
+++ R/parameters.R
@@ -1,18 +1,21 @@
-set_initial_parameters <- function(p, B, n, y, X, Z, init.sigma, init.length_scale=20, verbose=FALSE) {
+set_initial_parameters <- function(p, B, n, y, X, Z, init.sigma=NULL, init.length_scale=20, verbose=FALSE) {
   Z <- as.matrix(Z)
   X <- as.matrix(X)
-  if (!missing(init.sigma)) {
+  if (is.null(init.sigma)) {
     #least square
     QR <- qr.default(cbind(X,Z,1))
     Q <- qr.qy(QR, diag(1, nrow = nrow(QR$qr), ncol = QR$rank))
     init.sigma <- log( t(y) %*% (diag(n) - tcrossprod(Q)) %*% y / (n - 1) )
+  }
+  else {
+    init.sigma <- log(init.sigma)
   }
   # RF:
   #rf_fit <- ranger::ranger(y ~ ., data=data.frame(y=y, X=X, Z=Z))
   #init_sigma <- log( sum( (y - rf_fit$predictions)^2) / (n-1)  )
 
   if (verbose) cat("Initial noise variance: ", exp(init.sigma), "\n")
-  parameters <- matrix(c(init.sigma, #sigma
+  parameters <- matrix(c(init.sigma, # "log( sigma)"
                          0, # mu
                          -log(rep(1, B)),# kernel-scales
                          log(rep(init.length_scale, B * p)) # ARD length-scales
